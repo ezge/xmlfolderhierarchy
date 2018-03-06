84e24d0b6360958ee732c836d365452349ee0776
ezge <ezge.rea@gmail.com>
2018-03-06 23:14:16 +0100

    initial commit

---
+++ ApexSQLDemo/DisplayXml.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ApexSQLDemo
+{
+    partial class frmDisplayXml
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCreateXml = new System.Windows.Forms.Button();
+            this.txtFolderName = new System.Windows.Forms.TextBox();
+            this.lblFolderName = new System.Windows.Forms.Label();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnBrowseFolder = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnShowXml = new System.Windows.Forms.Button();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SaveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnBrowseFolder);
+            this.panel1.Controls.Add(this.txtFolderName);
+            this.panel1.Controls.Add(this.lblFolderName);
+            this.panel1.Location = new System.Drawing.Point(13, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(453, 66);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnCreateXml
+            // 
+            this.btnCreateXml.Location = new System.Drawing.Point(358, 20);
+            this.btnCreateXml.Name = "btnCreateXml";
+            this.btnCreateXml.Size = new System.Drawing.Size(81, 23);
+            this.btnCreateXml.TabIndex = 2;
+            this.btnCreateXml.Text = "Create Xml";
+            this.btnCreateXml.UseVisualStyleBackColor = true;
+            this.btnCreateXml.Click += new System.EventHandler(this.btnCreateXml_Click);
+            // 
+            // txtFolderName
+            // 
+            this.txtFolderName.Location = new System.Drawing.Point(73, 11);
+            this.txtFolderName.Name = "txtFolderName";
+            this.txtFolderName.ReadOnly = true;
+            this.txtFolderName.Size = new System.Drawing.Size(366, 20);
+            this.txtFolderName.TabIndex = 1;
+            // 
+            // lblFolderName
+            // 
+            this.lblFolderName.AutoSize = true;
+            this.lblFolderName.Location = new System.Drawing.Point(3, 14);
+            this.lblFolderName.Name = "lblFolderName";
+            this.lblFolderName.Size = new System.Drawing.Size(64, 13);
+            this.lblFolderName.TabIndex = 0;
+            this.lblFolderName.Text = "Folder Path:";
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.SelectedPath = "C:\\Users\\Rea\\Downloads";
+            // 
+            // btnBrowseFolder
+            // 
+            this.btnBrowseFolder.Location = new System.Drawing.Point(358, 38);
+            this.btnBrowseFolder.Name = "btnBrowseFolder";
+            this.btnBrowseFolder.Size = new System.Drawing.Size(81, 23);
+            this.btnBrowseFolder.TabIndex = 3;
+            this.btnBrowseFolder.Text = "Open Folder";
+            this.btnBrowseFolder.UseVisualStyleBackColor = true;
+            this.btnBrowseFolder.Click += new System.EventHandler(this.btnBrowseFolder_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.treeView);
+            this.panel2.Controls.Add(this.btnCreateXml);
+            this.panel2.Controls.Add(this.btnShowXml);
+            this.panel2.Location = new System.Drawing.Point(13, 84);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(453, 338);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnShowXml
+            // 
+            this.btnShowXml.Location = new System.Drawing.Point(358, 49);
+            this.btnShowXml.Name = "btnShowXml";
+            this.btnShowXml.Size = new System.Drawing.Size(81, 23);
+            this.btnShowXml.TabIndex = 0;
+            this.btnShowXml.Text = "Load Xml";
+            this.btnShowXml.UseVisualStyleBackColor = true;
+            this.btnShowXml.Click += new System.EventHandler(this.btnShowXml_Click);
+            // 
+            // treeView
+            // 
+            this.treeView.Location = new System.Drawing.Point(6, 16);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(346, 305);
+            this.treeView.TabIndex = 1;
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.AddExtension = false;
+            this.OpenFileDialog.DefaultExt = "xml";
+            this.OpenFileDialog.Filter = "XML files (*.xml)|*.xml";
+            this.OpenFileDialog.InitialDirectory = "c:\\";
+            // 
+            // SaveFileDialog
+            // 
+            this.SaveFileDialog.DefaultExt = "xml";
+            this.SaveFileDialog.Filter = "XML files (*.xml)|*.xml";
+            // 
+            // frmDisplayXml
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 434);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmDisplayXml";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Display Xml";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCreateXml;
+        private System.Windows.Forms.TextBox txtFolderName;
+        private System.Windows.Forms.Label lblFolderName;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button btnBrowseFolder;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.Button btnShowXml;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.SaveFileDialog SaveFileDialog;
+    }
+}
+
---
+++ ApexSQLDemo/DisplayXml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace ApexSQLDemo
+{
+    public partial class frmDisplayXml : Form
+    {
+        public frmDisplayXml()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateXml_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(txtFolderName.Text))
+            {
+                if (SaveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Cursor.Current = Cursors.WaitCursor;
+                    DirectoryInfo dir = new DirectoryInfo(txtFolderName.Text);
+                    var doc = new XDocument(new XDeclaration("1.0", "UTF-8", "yes"), CreateXmlFile(dir));
+                    doc.Save(SaveFileDialog.FileName);
+                    Cursor.Current = Cursors.Default;
+                }
+            }
+            else MessageBox.Show("Folder name is not a valid."); 
+        }
+
+        private static XElement CreateXmlFile(DirectoryInfo dir)
+        {
+            //get directories
+            var xmlInfo = new XElement("folder",
+                new XElement("name", dir.Name),
+                new XElement("size", GetDirectorySize(dir)),
+                new XElement("numoffiles", dir.GetFiles().Length));
+
+            //get subdirectories
+            foreach (var subDir in dir.GetDirectories())
+            {
+                xmlInfo.Add(CreateXmlFile(subDir));
+            }
+
+            //get all the files
+            foreach (var file in dir.GetFiles())
+            {
+                xmlInfo.Add(new XElement("file",
+                    new XElement("name", file.Name),
+                    new XElement("size", file.Length),
+                    new XElement("creationTime", file.CreationTime),
+                    new XElement("lastAccess", file.LastAccessTime),
+                    new XElement("lastModify", file.LastWriteTime)));
+            }
+            return xmlInfo;
+            
+        }
+
+        private static long GetDirectorySize(DirectoryInfo dir)
+        {
+            long size = 0;
+            // Add file sizes.
+            FileInfo[] fis = dir.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+            // Add subdirectory sizes.
+            DirectoryInfo[] dis = dir.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += GetDirectorySize(di);
+            }
+            return size;
+        }
+
+        private void btnBrowseFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFolderName.Text = folderBrowserDialog.SelectedPath;
+            } 
+        }
+
+        private void btnShowXml_Click(object sender, EventArgs e)
+        {
+            if (OpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Cursor.Current = Cursors.WaitCursor; 
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(OpenFileDialog.FileName);
+
+                    treeView.Nodes.Clear();
+                    foreach (XmlNode node in doc)
+                    {
+                        TreeNode treeNode = XmlNode2TreeNode(node);
+                        //GetAttributes(node, treeNode);
+                        treeNode.Text = FormatName(node);
+                       // treeNode.Text = node.Name;
+                        treeView.Nodes.Add(treeNode);
+                    }
+
+                    treeView.ExpandAll();
+                    Cursor.Current = Cursors.Default;  
+                }
+                catch (XmlException xmlEx)
+                {
+                    Cursor.Current = Cursors.Default; 
+                    MessageBox.Show(xmlEx.Message);
+                }
+                catch (Exception ex)
+                {
+                    Cursor.Current = Cursors.Default; 
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private TreeNode XmlNode2TreeNode(XmlNode parentNode)
+        {
+            TreeNode treeNode = new TreeNode();
+
+            foreach (XmlNode childNode in parentNode.ChildNodes)
+            {
+                if (childNode.NodeType == XmlNodeType.Element)
+                {
+                    TreeNode childTreeNode = new TreeNode();
+
+                    if (childNode.ChildNodes.Count > 0)
+                        childTreeNode = XmlNode2TreeNode(childNode);
+
+                    childTreeNode.Text = FormatName(childNode);
+                    treeNode.Nodes.Add(childTreeNode);
+                }
+            }
+
+            return treeNode;
+        }
+
+        private string FormatName(XmlNode node)
+        {
+            string fullName = "<" + node.Name;
+
+            if (node.Name.Contains("folder") || node.Name.Contains("file"))
+                fullName += ">";
+            else
+            {
+                if (node.InnerText != "")
+                    fullName += ">" + node.InnerText + "</" + node.Name + ">";
+                else
+                    fullName += ">";
+            }
+            return fullName;
+        }
+
+}
+      
+    
+}
